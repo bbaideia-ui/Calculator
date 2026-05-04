8fd40adfba89288bec6ce17f45f2e05c02bd55be
bbaideia-ui <bba.ideia@gmail.com>
2026-05-04 19:59:55 -0400

    Update theme.js

--- theme/theme.js
+++ theme/theme.js
@@ -1,1 +1,32 @@
 
+const themeToggle = document.getElementById("themeToggle");
+
+const savedTheme = localStorage.getItem("financeToolsTheme");
+
+if(savedTheme === "dark"){
+  document.body.classList.add("dark");
+  themeToggle.textContent = "☀️ Light";
+}else{
+  document.body.classList.remove("dark");
+  themeToggle.textContent = "🌙 Dark";
+}
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.add("fade-transition");
+
+  setTimeout(() => {
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+      themeToggle.textContent = "☀️ Light";
+      localStorage.setItem("financeToolsTheme", "dark");
+    }else{
+      themeToggle.textContent = "🌙 Dark";
+      localStorage.setItem("financeToolsTheme", "light");
+    }
+
+    requestAnimationFrame(() => {
+      document.body.classList.remove("fade-transition");
+    });
+  }, 140);
+});
