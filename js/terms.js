aeaa14851d4d93dbd60f914971a2af9f60d60d07
bbaideia-ui <bba.ideia@gmail.com>
2026-05-11 11:56:05 -0400

    Implement menu toggle functionality
    Added functionality to toggle the main navigation menu.

---
+++ js/terms.js
@@ -0,0 +1,7 @@
+
+const menuToggle = document.getElementById("menuToggle");
+const mainNav = document.getElementById("mainNav");
+
+menuToggle.addEventListener("click", () => {
+  mainNav.classList.toggle("open");
+});
