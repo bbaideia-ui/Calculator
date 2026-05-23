b5c855987ea59dcbea75775c5f53652608621091
bbaideia-ui <bba.ideia@gmail.com>
2026-05-22 22:58:09 -0400

    Update all-calculator.js

--- js/all-calculator.js
+++ js/all-calculator.js
@@ -204,16 +204,3 @@ document.addEventListener("click", (event) => {
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById("menuToggle");
-  const mainNav = document.getElementById("mainNav");
-
-  if(menuToggle && mainNav){
-    menuToggle.addEventListener("click", event => {
-      event.preventDefault();
-      event.stopPropagation();
-
-      mainNav.classList.toggle("open");
-    });
-  }
-});
