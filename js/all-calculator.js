2d218841727c39a499f9ab56f15840de63004fea
bbaideia-ui <bba.ideia@gmail.com>
2026-05-22 22:56:30 -0400

    Update all-calculator.js

--- js/all-calculator.js
+++ js/all-calculator.js
@@ -197,3 +197,23 @@ document.addEventListener("click", (event) => {
   }
 
 });
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menuToggle");
+  const mainNav = document.getElementById("mainNav");
+
+  if(menuToggle && mainNav){
+    menuToggle.addEventListener("click", event => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      mainNav.classList.toggle("open");
+    });
+  }
+});
